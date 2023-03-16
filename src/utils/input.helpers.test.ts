2b2593bf052cf087c998cdf2815c51ec3be0e025
Zong-Rong Huang <zongrong.h886@gmail.com>
2023-03-16 21:50:43 +0800

    chore: add typescript and jest

---
+++ src/utils/input.helpers.test.ts
@@ -0,0 +1,12 @@
+import {
+  clearCurrentInputHelper,
+  negateLastNumberHelper,
+  keyinHelper,
+  normalizeOperatorsHelper,
+} from "./input.helpers";
+
+describe("[Input Helpers]", () => {
+  test("first", () => {
+    expect(3 + 2).toBe(5);
+  });
+});
