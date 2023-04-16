b28e4dfda3b86ce6c5b377ec0ec188239f99419b
Zong-Rong Huang <zongrong.h886@gmail.com>
2023-04-16 22:46:20 +0800

    test: add more tests for input.js

--- src/utils/input.test.ts
+++ src/utils/input.test.ts
@@ -1,31 +1,413 @@
-import { combineMathExp, normalizeInput } from "./input";
+import {
+  makeOppositeNum,
+  normalizeInput,
+  ignoreExpWithEqualSign,
+  addDecimalDot,
+  addOperator,
+  addNumber,
+} from "./input";
 
 describe("[Make valid math expressions]", () => {
   test("Normalize input", () => {
-    const inputs = ["=", "+", "-", "x", "÷", ".", "1", "+/-", "0"];
-    const expectedValues = [
-      " = ",
-      " + ",
-      " - ",
-      " x ",
-      " ÷ ",
-      ".",
-      "1",
-      "+/-",
-      "0",
-    ];
-    expect(inputs.map((input) => normalizeInput(input))).toEqual(
-      expectedValues
-    );
+    const inputs = ["=", "+", "-", "x", "÷", ".", "1", "0"];
+    const expected = [" = ", " + ", " - ", " x ", " ÷ ", ".", "1", "0"];
+    expect(inputs.map((input) => normalizeInput(input))).toEqual(expected);
   });
 
   test("Add nothing after =", () => {
-    const baseExp = "2 + 3 = ";
-    const inputs = ["=", "+", "-", "x", "÷", ".", "1", "+/-", "0"];
-    const expectedValue = Array(inputs.length).fill(baseExp);
+    const exps = [
+      "0 = ",
+      "-1 = ",
+      "2.3 = ",
+      "2 + 1 = ",
+      "2 - 1 = ",
+      "2 x 1 = ",
+      "2 x -1 = ",
+      "2 ÷ 1 = ",
+      "2 ÷ -1 = ",
+    ];
 
-    expect(inputs.map((input) => combineMathExp(baseExp, input))).toEqual(
-      expectedValue
-    );
+    expect(exps.map((exp) => ignoreExpWithEqualSign(exp))).toEqual(exps);
+  });
+
+  test("+/- makes opposite numbers", () => {
+    const map = {
+      "": "",
+      "1": "-1",
+      "-1": "1",
+      "0": "0",
+      "1.25": "-1.25",
+      "-1.25": "1.25",
+
+      "2 + 1": "2 + -1",
+      "2 - 1": "2 - -1",
+      "2 x 1": "2 x -1",
+      "2 x -1": "2 x 1",
+      "2 ÷ 1": "2 ÷ -1",
+      "2 ÷ -1": "2 ÷ 1",
+
+      "2 + 1.2": "2 + -1.2",
+      "2 - 1.2": "2 - -1.2",
+      "2 - -1.2": "2 - 1.2",
+      "2 x 1.2": "2 x -1.2",
+      "2 x -1.2": "2 x 1.2",
+      "2 ÷ 1.2": "2 ÷ -1.2",
+      "2 ÷ -1.2": "2 ÷ 1.2",
+
+      // 最後為 operator，不做處理
+      "2 + 1 = ": "2 + 1 = ",
+      "2 + 1 + ": "2 + 1 + ",
+      "2 + 1 - ": "2 + 1 - ",
+      "2 + 1 x ": "2 + 1 x ",
+      "2 + 1 ÷ ": "2 + 1 ÷ ",
+
+      // 小數點結尾
+      "2 + 1.": "2 + -1",
+      "2 + -1.": "2 + 1",
+    };
+
+    const baseExps = Object.keys(map);
+    const expected = Object.values(map);
+    expect(baseExps.map((exp) => makeOppositeNum(exp))).toEqual(expected);
+  });
+
+  test("Add decimal dot", () => {
+    const map = {
+      "": "",
+      "1": "1.",
+      "-1": "-1.",
+      "0": "0.",
+      "1.25": "1.25",
+      "-1.25": "-1.25",
+
+      "2 + 1": "2 + 1.",
+      "2 - 1": "2 - 1.",
+      "2 x 1": "2 x 1.",
+      "2 x -1": "2 x -1.",
+      "2 ÷ 1": "2 ÷ 1.",
+      "2 ÷ -1": "2 ÷ -1.",
+
+      // 最後數字含小數點，不做處理
+      "2 + 1.2": "2 + 1.2",
+      "2 - 1.2": "2 - 1.2",
+      "2 - -1.2": "2 - -1.2",
+      "2 x 1.2": "2 x 1.2",
+      "2 x -1.2": "2 x -1.2",
+      "2 ÷ 1.2": "2 ÷ 1.2",
+      "2 ÷ -1.2": "2 ÷ -1.2",
+
+      // 最後為 operator，不做處理
+      "2 + 1 = ": "2 + 1 = ",
+      "2 + 1 + ": "2 + 1 + ",
+      "2 + 1 - ": "2 + 1 - ",
+      "2 + 1 x ": "2 + 1 x ",
+      "2 + 1 ÷ ": "2 + 1 ÷ ",
+    };
+
+    const baseExps = Object.keys(map);
+    const expected = Object.values(map);
+    expect(baseExps.map((exp) => addDecimalDot(exp))).toEqual(expected);
+  });
+
+  test("Add operator +", () => {
+    const operator = "+";
+    const map = {
+      "": "",
+      "1": `1 ${operator} `,
+      "-1": `-1 ${operator} `,
+      "0": `0 ${operator} `,
+      "1.2": `1.2 ${operator} `,
+      "-1.2": `-1.2 ${operator} `,
+
+      // 最後為 .，移除 . 並加上 operator
+      "2 + 1.": `2 + 1 ${operator} `,
+      "2 - 1.": `2 - 1 ${operator} `,
+      "2 x 1.": `2 x 1 ${operator} `,
+      "2 x -1.": `2 x -1 ${operator} `,
+      "2 ÷ 1.": `2 ÷ 1 ${operator} `,
+      "2 ÷ -1.": `2 ÷ -1 ${operator} `,
+
+      // 最後為 =，不做處理
+      "2 + 1 = ": "2 + 1 = ",
+
+      // 最後為 operator，替換成新的 operator
+      "2 + 1 + ": `2 + 1 ${operator} `,
+      "2 + 1 - ": `2 + 1 ${operator} `,
+      "2 + 1 x ": `2 + 1 ${operator} `,
+      "2 + 1 ÷ ": `2 + 1 ${operator} `,
+    };
+
+    const baseExps = Object.keys(map);
+    const expected = Object.values(map);
+
+    expect(baseExps.map((exp) => addOperator(exp, operator))).toEqual(expected);
+  });
+
+  test("Add operator -", () => {
+    const operator = "-";
+    const map = {
+      "": "",
+      "1": `1 ${operator} `,
+      "-1": `-1 ${operator} `,
+      "0": `0 ${operator} `,
+      "1.2": `1.2 ${operator} `,
+      "-1.2": `-1.2 ${operator} `,
+
+      // 最後為 .，移除 . 並加上 operator
+      "2 + 1.": `2 + 1 ${operator} `,
+      "2 - 1.": `2 - 1 ${operator} `,
+      "2 x 1.": `2 x 1 ${operator} `,
+      "2 x -1.": `2 x -1 ${operator} `,
+      "2 ÷ 1.": `2 ÷ 1 ${operator} `,
+      "2 ÷ -1.": `2 ÷ -1 ${operator} `,
+
+      // 最後為 =，不做處理
+      "2 + 1 = ": "2 + 1 = ",
+
+      // 最後為 operator，替換成新的 operator
+      "2 + 1 + ": `2 + 1 ${operator} `,
+      "2 + 1 - ": `2 + 1 ${operator} `,
+      "2 + 1 x ": `2 + 1 ${operator} `,
+      "2 + 1 ÷ ": `2 + 1 ${operator} `,
+    };
+
+    const baseExps = Object.keys(map);
+    const expected = Object.values(map);
+
+    expect(baseExps.map((exp) => addOperator(exp, operator))).toEqual(expected);
+  });
+
+  test("Add operator x", () => {
+    const operator = "x";
+    const map = {
+      "": "",
+      "1": `1 ${operator} `,
+      "-1": `-1 ${operator} `,
+      "0": `0 ${operator} `,
+      "1.2": `1.2 ${operator} `,
+      "-1.2": `-1.2 ${operator} `,
+
+      // 最後為 .，移除 . 並加上 operator
+      "2 + 1.": `2 + 1 ${operator} `,
+      "2 - 1.": `2 - 1 ${operator} `,
+      "2 x 1.": `2 x 1 ${operator} `,
+      "2 x -1.": `2 x -1 ${operator} `,
+      "2 ÷ 1.": `2 ÷ 1 ${operator} `,
+      "2 ÷ -1.": `2 ÷ -1 ${operator} `,
+
+      // 最後為 =，不做處理
+      "2 + 1 = ": "2 + 1 = ",
+
+      // 最後為 operator，替換成新的 operator
+      "2 + 1 + ": `2 + 1 ${operator} `,
+      "2 + 1 - ": `2 + 1 ${operator} `,
+      "2 + 1 x ": `2 + 1 ${operator} `,
+      "2 + 1 ÷ ": `2 + 1 ${operator} `,
+    };
+
+    const baseExps = Object.keys(map);
+    const expected = Object.values(map);
+
+    expect(baseExps.map((exp) => addOperator(exp, operator))).toEqual(expected);
+  });
+
+  test("Add operator ÷", () => {
+    const operator = "÷";
+    const map = {
+      "": "",
+      "1": `1 ${operator} `,
+      "-1": `-1 ${operator} `,
+      "0": `0 ${operator} `,
+      "1.2": `1.2 ${operator} `,
+      "-1.2": `-1.2 ${operator} `,
+
+      // 最後為 .，移除 . 並加上 operator
+      "2 + 1.": `2 + 1 ${operator} `,
+      "2 - 1.": `2 - 1 ${operator} `,
+      "2 x 1.": `2 x 1 ${operator} `,
+      "2 x -1.": `2 x -1 ${operator} `,
+      "2 ÷ 1.": `2 ÷ 1 ${operator} `,
+      "2 ÷ -1.": `2 ÷ -1 ${operator} `,
+
+      // 最後為 =，不做處理
+      "2 + 1 = ": "2 + 1 = ",
+
+      // 最後為 operator，替換成新的 operator
+      "2 + 1 + ": `2 + 1 ${operator} `,
+      "2 + 1 - ": `2 + 1 ${operator} `,
+      "2 + 1 x ": `2 + 1 ${operator} `,
+      "2 + 1 ÷ ": `2 + 1 ${operator} `,
+    };
+
+    const baseExps = Object.keys(map);
+    const expected = Object.values(map);
+
+    expect(baseExps.map((exp) => addOperator(exp, operator))).toEqual(expected);
+  });
+
+  test("Add operator =", () => {
+    const operator = "=";
+    const map = {
+      "": "",
+      "1": `1 ${operator} `,
+      "-1": `-1 ${operator} `,
+      "0": `0 ${operator} `,
+      "1.2": `1.2 ${operator} `,
+      "-1.2": `-1.2 ${operator} `,
+
+      // 最後為 .，移除 . 並加上 operator
+      "2 + 1.": `2 + 1 ${operator} `,
+      "2 - 1.": `2 - 1 ${operator} `,
+      "2 x 1.": `2 x 1 ${operator} `,
+      "2 x -1.": `2 x -1 ${operator} `,
+      "2 ÷ 1.": `2 ÷ 1 ${operator} `,
+      "2 ÷ -1.": `2 ÷ -1 ${operator} `,
+
+      // 最後為 =，不做處理
+      "2 + 1 = ": "2 + 1 = ",
+
+      // 最後為 operator，替換成新的 operator
+      "2 + 1 + ": `2 + 1 ${operator} `,
+      "2 + 1 - ": `2 + 1 ${operator} `,
+      "2 + 1 x ": `2 + 1 ${operator} `,
+      "2 + 1 ÷ ": `2 + 1 ${operator} `,
+    };
+
+    const baseExps = Object.keys(map);
+    const expected = Object.values(map);
+
+    expect(baseExps.map((exp) => addOperator(exp, operator))).toEqual(expected);
+  });
+
+  test("Add number 0", () => {
+    const num = "0";
+    const map = {
+      "": "0",
+      "0": "0",
+      "1": "10",
+      "5": "50",
+      "90": "900",
+      "-1": "-10",
+      "-5": "-50",
+      "-90": "-900",
+      "1.2": "1.20",
+      "1.20": "1.200",
+      "-1.2": "-1.20",
+      "-1.20": "-1.200",
+
+      "2 + 1": "2 + 10",
+      "2 - 1": "2 - 10",
+      "2 x 1": "2 x 10",
+      "2 x -1": "2 x -10",
+      "2 ÷ 1": "2 ÷ 10",
+      "2 ÷ -1": "2 ÷ -10",
+
+      "2 + 1.": "2 + 1.0",
+      "2 - 1.": "2 - 1.0",
+      "2 x 1.": "2 x 1.0",
+      "2 x -1.": "2 x -1.0",
+      "2 ÷ 1.": "2 ÷ 1.0",
+      "2 ÷ -1.": "2 ÷ -1.0",
+
+      // 最後為 =，不做處理
+      "2 + 1 = ": "2 + 1 = ",
+
+      "2 + 1 + ": "2 + 1 + 0",
+      "2 + 1 - ": "2 + 1 - 0",
+      "2 + 1 x ": "2 + 1 x 0",
+      "2 + 1 ÷ ": "2 + 1 ÷ 0",
+
+      // 數字開頭最多一個 0
+      "2 + 1 + 0": "2 + 1 + 0",
+      "2 + 1 - 0": "2 + 1 - 0",
+      "2 + 1 x 0": "2 + 1 x 0",
+      "2 + 1 ÷ 0": "2 + 1 ÷ 0",
+
+      // 小數點後可加 0
+      "2 + 1 + 0.": "2 + 1 + 0.0",
+      "2 + 1 - 0.": "2 + 1 - 0.0",
+      "2 + 1 x 0.": "2 + 1 x 0.0",
+      "2 + 1 ÷ 0.": "2 + 1 ÷ 0.0",
+
+      "2 + 1 + 0.1": "2 + 1 + 0.10",
+      "2 + 1 - 0.1": "2 + 1 - 0.10",
+      "2 + 1 x 0.1": "2 + 1 x 0.10",
+      "2 + 1 ÷ 0.1": "2 + 1 ÷ 0.10",
+
+      "2 + 1 + 0.0": "2 + 1 + 0.00",
+      "2 + 1 - 0.0": "2 + 1 - 0.00",
+      "2 + 1 x 0.0": "2 + 1 x 0.00",
+      "2 + 1 ÷ 0.0": "2 + 1 ÷ 0.00",
+    };
+
+    const baseExps = Object.keys(map);
+    const expected = Object.values(map);
+    expect(baseExps.map((exp) => addNumber(exp, num))).toEqual(expected);
+  });
+
+  test("Add number 1-9", () => {
+    const num = "2";
+    const map = {
+      "": "2",
+      "0": "2",
+      "1": "12",
+      "5": "52",
+      "90": "902",
+      "-1": "-12",
+      "-5": "-52",
+      "-90": "-902",
+      "1.2": "1.22",
+      "1.20": "1.202",
+      "-1.2": "-1.22",
+      "-1.20": "-1.202",
+
+      "2 + 1": "2 + 12",
+      "2 - 1": "2 - 12",
+      "2 x 1": "2 x 12",
+      "2 x -1": "2 x -12",
+      "2 ÷ 1": "2 ÷ 12",
+      "2 ÷ -1": "2 ÷ -12",
+
+      "2 + 1.": "2 + 1.2",
+      "2 - 1.": "2 - 1.2",
+      "2 x 1.": "2 x 1.2",
+      "2 x -1.": "2 x -1.2",
+      "2 ÷ 1.": "2 ÷ 1.2",
+      "2 ÷ -1.": "2 ÷ -1.2",
+
+      // 最後為 =，不做處理
+      "2 + 1 = ": "2 + 1 = ",
+
+      "2 + 1 + ": "2 + 1 + 2",
+      "2 + 1 - ": "2 + 1 - 2",
+      "2 + 1 x ": "2 + 1 x 2",
+      "2 + 1 ÷ ": "2 + 1 ÷ 2",
+
+      // 數字開頭最多一個 0
+      "2 + 1 + 0": "2 + 1 + 2",
+      "2 + 1 - 0": "2 + 1 - 2",
+      "2 + 1 x 0": "2 + 1 x 2",
+      "2 + 1 ÷ 0": "2 + 1 ÷ 2",
+
+      // 小數點後可加 0
+      "2 + 1 + 0.": "2 + 1 + 0.2",
+      "2 + 1 - 0.": "2 + 1 - 0.2",
+      "2 + 1 x 0.": "2 + 1 x 0.2",
+      "2 + 1 ÷ 0.": "2 + 1 ÷ 0.2",
+
+      "2 + 1 + 0.1": "2 + 1 + 0.12",
+      "2 + 1 - 0.1": "2 + 1 - 0.12",
+      "2 + 1 x 0.1": "2 + 1 x 0.12",
+      "2 + 1 ÷ 0.1": "2 + 1 ÷ 0.12",
+
+      "2 + 1 + 0.0": "2 + 1 + 0.02",
+      "2 + 1 - 0.0": "2 + 1 - 0.02",
+      "2 + 1 x 0.0": "2 + 1 x 0.02",
+      "2 + 1 ÷ 0.0": "2 + 1 ÷ 0.02",
+    };
+
+    const baseExps = Object.keys(map);
+    const expected = Object.values(map);
+    expect(baseExps.map((exp) => addNumber(exp, num))).toEqual(expected);
   });
 });
--- src/utils/input.ts
+++ src/utils/input.ts
@@ -2,150 +2,165 @@ type Keys = {
   [key: string]: string;
 };
 
-const clearCurrentInputHelper = (formula: string) => {
-  const splitFormula = formula.split(" ");
-  const lastItemId = splitFormula.length - 1;
-  const lastItem = splitFormula[lastItemId];
-  let newFormula = "";
+// const clearCurrentInputHelper = (formula: string) => {
+//   const splitFormula = formula.split(" ");
+//   const lastItemId = splitFormula.length - 1;
+//   const lastItem = splitFormula[lastItemId];
+//   let newFormula = "";
 
-  // 最後一項是數字字串
-  if (lastItem !== "") {
-    newFormula = splitFormula.reduce((base, item, id) => {
-      return id === lastItemId ? base : `${base}${item} `;
-    }, "");
-  }
+//   // 最後一項是數字字串
+//   if (lastItem !== "") {
+//     newFormula = splitFormula.reduce((base, item, id) => {
+//       return id === lastItemId ? base : `${base}${item} `;
+//     }, "");
+//   }
 
-  // 最後一項是 ''，代表 formula 是以 operator 結束
-  if (lastItem === "") {
-    newFormula = splitFormula
-      .reduce((base, item, id) => {
-        return id >= lastItemId - 1 ? base : `${base}${item} `;
-      }, "")
-      .trimEnd();
-  }
-  console.log("clearnCurrentInputHelper", { newFormula });
-  return newFormula ? newFormula : "0";
-};
+//   // 最後一項是 ''，代表 formula 是以 operator 結束
+//   if (lastItem === "") {
+//     newFormula = splitFormula
+//       .reduce((base, item, id) => {
+//         return id >= lastItemId - 1 ? base : `${base}${item} `;
+//       }, "")
+//       .trimEnd();
+//   }
+//   console.log("clearnCurrentInputHelper", { newFormula });
+//   return newFormula ? newFormula : "0";
+// };
 
-const negateLastNumberHelper = (formula: string) => {
-  const isOneZero = formula === "0";
-  const endsWithSpaceZero = formula.endsWith(" 0");
-  const endsWithNegator = formula.endsWith("-");
-  const endsWithEqual = formula.endsWith(" = ");
-  const endsWithZeroDecimal = new RegExp(/0\.0*$/).test(formula);
-  const endsWithOperator = new RegExp(/\s[+x÷-]\s$/).test(formula);
+// const negateLastNumberHelper = (formula: string) => {
+//   const isOneZero = formula === "0";
+//   const endsWithSpaceZero = formula.endsWith(" 0");
+//   const endsWithNegator = formula.endsWith("-");
+//   const endsWithEqual = formula.endsWith(" = ");
+//   const endsWithZeroDecimal = new RegExp(/0\.0*$/).test(formula);
+//   const endsWithOperator = new RegExp(/\s[+x÷-]\s$/).test(formula);
 
-  const cannotNegateLastItem =
-    isOneZero ||
-    endsWithSpaceZero ||
-    endsWithZeroDecimal ||
-    endsWithEqual ||
-    endsWithNegator;
+//   const cannotNegateLastItem =
+//     isOneZero ||
+//     endsWithSpaceZero ||
+//     endsWithZeroDecimal ||
+//     endsWithEqual ||
+//     endsWithNegator;
 
-  if (cannotNegateLastItem) {
-    return formula;
-  }
+//   if (cannotNegateLastItem) {
+//     return formula;
+//   }
 
-  if (endsWithOperator) {
-    return formula + "-";
-  }
+//   if (endsWithOperator) {
+//     return formula + "-";
+//   }
 
-  // 結尾為非零的數字，非零的數字做成相反數
-  if (!endsWithOperator) {
-    const splitFormula = formula.split(" ");
-    const lastItem = splitFormula[splitFormula.length - 1];
-    const negatedNumberString = (-Number(lastItem)).toString();
+//   // 結尾為非零的數字，非零的數字做成相反數
+//   if (!endsWithOperator) {
+//     const splitFormula = formula.split(" ");
+//     const lastItem = splitFormula[splitFormula.length - 1];
+//     const negatedNumberString = (-Number(lastItem)).toString();
 
-    splitFormula.pop();
-    splitFormula.push(negatedNumberString);
-    return splitFormula.join(" ");
-  }
-};
+//     splitFormula.pop();
+//     splitFormula.push(negatedNumberString);
+//     return splitFormula.join(" ");
+//   }
+// };
 
-const keyinHelper = (formula: string, btnText: string) => {
-  console.log({ formula, btnText });
+// const keyinHelper = (formula: string, btnText: string) => {
+//   console.log({ formula, btnText });
 
-  const endsWithDecimalDot = formula.endsWith(".");
-  const endsWithOperator = new RegExp(/\s[+x÷-]\s$/).test(formula);
-  const endsWithNegator = formula.endsWith("-");
-  const endsWithSpaceZero = formula.endsWith(" 0");
-  const hasOnlyOneZero = formula.length === 1 && formula[0] === "0";
-  const endsWithEqual = formula.endsWith(" = ");
-  const endsWithDecimalNumber = new RegExp(/\.\d+$/).test(formula);
+//   const endsWithDecimalDot = formula.endsWith(".");
+//   const endsWithOperator = new RegExp(/\s[+x÷-]\s$/).test(formula);
+//   const endsWithNegator = formula.endsWith("-");
+//   const endsWithSpaceZero = formula.endsWith(" 0");
+//   const hasOnlyOneZero = formula.length === 1 && formula[0] === "0";
+//   const endsWithEqual = formula.endsWith(" = ");
+//   const endsWithDecimalNumber = new RegExp(/\.\d+$/).test(formula);
 
-  switch (btnText) {
-    case ".":
-      const cannotAddDecimalDot =
-        endsWithDecimalNumber ||
-        endsWithDecimalDot ||
-        endsWithOperator ||
-        endsWithEqual;
-      console.log("keyinHelper [.]", { formula });
-      return cannotAddDecimalDot ? formula : `${formula}${btnText}`;
-    case " ÷ ":
-    case " - ":
-    case " + ":
-    case " x ":
-    case " = ":
-      const cannotAddOperator =
-        endsWithDecimalDot ||
-        endsWithOperator ||
-        endsWithEqual ||
-        endsWithNegator;
-      return cannotAddOperator ? formula : `${formula}${btnText}`;
-    case "0":
-      if (endsWithEqual) {
-        return btnText;
-      }
+//   switch (btnText) {
+//     case ".":
+//       const cannotAddDecimalDot =
+//         endsWithDecimalNumber ||
+//         endsWithDecimalDot ||
+//         endsWithOperator ||
+//         endsWithEqual;
+//       console.log("keyinHelper [.]", { formula });
+//       return cannotAddDecimalDot ? formula : `${formula}${btnText}`;
+//     case " ÷ ":
+//     case " - ":
+//     case " + ":
+//     case " x ":
+//     case " = ":
+//       const cannotAddOperator =
+//         endsWithDecimalDot ||
+//         endsWithOperator ||
+//         endsWithEqual ||
+//         endsWithNegator;
+//       return cannotAddOperator ? formula : `${formula}${btnText}`;
+//     case "0":
+//       if (endsWithEqual) {
+//         return btnText;
+//       }
 
-      const cannotAddZero = hasOnlyOneZero || endsWithSpaceZero;
-      console.log("keyinHelper [0]", { formula });
-      return cannotAddZero ? formula : `${formula}${btnText}`;
-    default:
-      // 輸入 1-9 字元
-      console.log("keyinHelper [default]", { formula });
-      if (endsWithEqual) {
-        return btnText;
-      }
+//       const cannotAddZero = hasOnlyOneZero || endsWithSpaceZero;
+//       console.log("keyinHelper [0]", { formula });
+//       return cannotAddZero ? formula : `${formula}${btnText}`;
+//     default:
+//       // 輸入 1-9 字元
+//       console.log("keyinHelper [default]", { formula });
+//       if (endsWithEqual) {
+//         return btnText;
+//       }
 
-      if (endsWithSpaceZero) {
-        const splitFormula = formula.split(" ");
-        splitFormula[splitFormula.length - 1] = btnText;
-        return splitFormula.join(" ");
-      }
-      return hasOnlyOneZero ? btnText : `${formula}${btnText}`;
-  }
-};
+//       if (endsWithSpaceZero) {
+//         const splitFormula = formula.split(" ");
+//         splitFormula[splitFormula.length - 1] = btnText;
+//         return splitFormula.join(" ");
+//       }
+//       return hasOnlyOneZero ? btnText : `${formula}${btnText}`;
+//   }
+// };
 
-const normalizeOperatorsHelper = (keyText: string) => {
-  const keyMappings: Keys = {
-    "=": " = ",
-    Enter: " = ",
-    "+": " + ",
-    "-": " - ",
-    "*": " x ",
-    "/": " ÷ ",
-  };
+// const normalizeOperatorsHelper = (keyText: string) => {
+//   const keyMappings: Keys = {
+//     "=": " = ",
+//     Enter: " = ",
+//     "+": " + ",
+//     "-": " - ",
+//     "*": " x ",
+//     "/": " ÷ ",
+//   };
 
-  console.log("[Normalize]", { keyText, display: keyMappings[keyText] });
-  return keyMappings[keyText] ? keyMappings[keyText] : keyText;
-};
+//   console.log("[Normalize]", { keyText, display: keyMappings[keyText] });
+//   return keyMappings[keyText] ? keyMappings[keyText] : keyText;
+// };
 
-function combineMathExp(prevExp: string, input: string) {
+function combineMathExp(baseExp: string, input: string) {
   const operators = ["+", "-", "x", "÷", "="];
-  const exp = prevExp.trim();
+  const exp = baseExp.trim();
   const finalChar = exp.at(-1) as string;
   const lastNum = exp.split(" ").at(-1) as string;
 
   // 結尾為 = 時，不可再加任何東西
   if (finalChar === "=") {
-    return prevExp;
+    return baseExp;
+  }
+
+  switch (input) {
+    case "+/-":
+
+    case "+":
+    case "-":
+    case "x":
+    case "÷":
+
+    case ".":
+
+    case "0":
+
+    default:
   }
 
   // 負數
   if (input === "+/-") {
     if (operators.includes(finalChar)) {
-      return prevExp;
+      return baseExp;
     }
     return `${-parseFloat(lastNum)}`;
   }
@@ -154,7 +169,7 @@ function combineMathExp(prevExp: string, input: string) {
   const startsWithZero =
     input === "0" && !lastNum?.includes(".") && lastNum?.startsWith("0");
   if (startsWithZero) {
-    return prevExp;
+    return baseExp;
   }
 
   // 不可兩個 operator (除了 -)；要以新 operator 取代舊 operator
@@ -167,7 +182,7 @@ function combineMathExp(prevExp: string, input: string) {
   // 不可 . operator
   const hasOperatorWithDot = finalChar === "." && operators.includes(input);
   if (hasOperatorWithDot) {
-    return prevExp;
+    return baseExp;
   }
 
   // 不可 operator + . || 不可 . .
@@ -175,10 +190,10 @@ function combineMathExp(prevExp: string, input: string) {
     (input === "." && operators.includes(finalChar)) ||
     (input === "." && finalChar === ".");
   if (cannotAddDecimalDot) {
-    return prevExp;
+    return baseExp;
   }
 
-  return prevExp + normalizeInput(input);
+  return baseExp + normalizeInput(input);
 }
 
 function normalizeInput(input: string) {
@@ -186,11 +201,100 @@ function normalizeInput(input: string) {
   return operators.includes(input) ? " " + input + " " : input;
 }
 
+function makeOppositeNum(baseExp: string) {
+  if (!baseExp) {
+    return baseExp;
+  }
+
+  const operators = ["+", "-", "x", "÷", "="];
+  const segments = baseExp.trim().split(" ");
+  const lastSegment = segments.at(-1) as string;
+  if (operators.includes(lastSegment)) {
+    return baseExp;
+  }
+
+  segments[segments.length - 1] = `${-parseFloat(lastSegment)}`;
+  return segments.join(" ");
+}
+
+function ignoreExpWithEqualSign(baseExp: string) {
+  if (baseExp.includes("=")) {
+    return baseExp;
+  }
+}
+
+function addDecimalDot(baseExp: string) {
+  if (!baseExp) {
+    return baseExp;
+  }
+
+  const segments = baseExp.trim().split(" ");
+  const lastSegment = segments.at(-1) as string;
+  const operators = ["+", "-", "x", "÷", "="];
+
+  if (operators.includes(lastSegment) || lastSegment.includes(".")) {
+    return baseExp;
+  }
+
+  segments[segments.length - 1] = lastSegment + ".";
+  return segments.join(" ");
+}
+
+function addOperator(baseExp: string, operator: string) {
+  if (!baseExp) {
+    return baseExp;
+  }
+
+  const segments = baseExp.trim().split(" ");
+  const lastSegment = segments.at(-1) as string;
+  const operators = ["+", "-", "x", "÷", "="];
+
+  if (lastSegment === "=") {
+    return baseExp;
+  }
+
+  if (operators.includes(lastSegment)) {
+    segments[segments.length - 1] = operator;
+    return [...segments, ""].join(" ");
+  }
+
+  if (lastSegment.endsWith(".")) {
+    const dotlessSegment = lastSegment.slice(0, -1);
+    segments[segments.length - 1] = dotlessSegment;
+  }
+
+  return [...segments, operator, ""].join(" ");
+}
+
+function addNumber(baseExp: string, input: string) {
+  if (!baseExp) {
+    return input;
+  }
+
+  const segments = baseExp.trim().split(" ");
+  const lastSegment = segments.at(-1) as string;
+
+  if (lastSegment === "=") {
+    return baseExp;
+  }
+
+  if (lastSegment === "0" && input === "0") {
+    return baseExp;
+  }
+
+  if (lastSegment === "0" && /[1-9]/.test(input)) {
+    segments[segments.length - 1] = input;
+    return segments.join(" ");
+  }
+
+  return baseExp + input;
+}
+
 export {
-  clearCurrentInputHelper,
-  negateLastNumberHelper,
-  keyinHelper,
-  normalizeOperatorsHelper,
-  combineMathExp,
+  makeOppositeNum,
   normalizeInput,
+  ignoreExpWithEqualSign,
+  addDecimalDot,
+  addOperator,
+  addNumber,
 };
