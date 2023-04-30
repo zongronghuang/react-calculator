2ddb9ec0755f12cd8eb44ba68f0881f132341505
Zong-Rong Huang <zongrong.h886@gmail.com>
2023-05-01 00:47:32 +0800

    feat: convert pressed keys to text

---
+++ src/utils/shortcuts.ts
@@ -0,0 +1,39 @@
+// keyboardEvent.key : 對應字串
+const shortcutMap: { [key: string]: string } = {
+  "0": "0",
+  "1": "1",
+  "2": "2",
+  "3": "3",
+  "4": "4",
+  "5": "5",
+  "6": "6",
+  "7": "7",
+  "8": "8",
+  "9": "9",
+  ".": ".",
+  "+": "+",
+  "-": "-",
+  "*": "x",
+  "/": "÷",
+  "=": "=",
+  "Enter": "=",
+  "Backspace": "C",
+  "Escape": "C",
+  "Clear": "C",
+};
+
+export default function keyToText(event: KeyboardEvent) {
+  const { altKey, key } = event;
+  console.log({ altKey, key });
+  const isACPressed = altKey && key === "Ç";
+  const isNegatorPressed = altKey && key === "-";
+
+  switch (true) {
+    case isACPressed:
+      return "AC";
+    case isNegatorPressed:
+      return "+/-";
+    default:
+      return shortcutMap[key] ? shortcutMap[key] : "";
+  }
+}
